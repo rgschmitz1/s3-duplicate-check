b5a7e0b43038fc9f8bde809af84c220fcb40fe99
Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>
2023-04-11 00:15:56 -0700

    Adding preliminary code
    Signed-off-by: Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>

---
+++ s3-duplicate-check.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+import boto3
+import json
+from botocore.exceptions import ClientError
+
+# Create an S3 client
+s3 = boto3.client('s3')
+
+# Get a list of S3 buckets
+buckets = s3.list_buckets()['Buckets']
+
+# Create a dictionary to store object hashes and their corresponding (bucket, key)
+object_table = dict()
+
+# Iterate through each bucket in the list
+for bucket in buckets:
+    # Get bucket name
+    bucket_name=bucket['Name']
+    #print(bucket_name)
+
+    # Get a list of all objects in the S3 bucket
+    try:
+        objects = s3.list_objects(Bucket=bucket_name)
+    except ClientError:
+        print(f'Bucket "{bucket_name}" does not appear to exist')
+        continue
+
+    # Check if bucket is empty (i.e. no contents)
+    if 'Contents' not in objects:
+        print(f'"{bucket_name}" is empty')
+        s3.delete_bucket(Bucket=bucket_name)
+        continue
+
+    # Iterate through each object in the bucket and store its hash and (bucket, key) tuple in the dictionary
+    for obj in objects['Contents']:
+        # Skip empty files
+        if not obj['Size']:
+            continue
+        etag = obj['ETag'].strip('"')
+        size = str(obj['Size'])
+        hash_value=','.join((etag, size))
+        key = obj['Key']
+        object_table.setdefault(hash_value, []).append((bucket_name, key))
+
+dupe_table=dict()
+# Iterate through the dictionary and delete any objects with multiple (bucket, key) tuples (i.e. duplicates)
+for hash_value, tuples in object_table.items():
+    if len(tuples) == 1:
+        continue
+
+    dupe_table[hash_value]=tuples
+
+    print(f'Deleting objects with hash value {hash_value}')
+
+    # Skip the first object in the list
+    for bucket_name, key in tuples[1:]:
+        print(f'Deleting key {key} from bucket {bucket_name}')
+        #s3.delete_object(Bucket=bucket_name, Key=key)
+
+# Write results to JSON
+with open('s3-duplicates.json', 'w') as file:
+    file.write(json.dumps(dupe_table))
